424895b0a0afd2f9901758e710d70833658d47e4
unknown <ashishbhandari380@gmail.com>
2026-02-10 20:58:14 +0545

    added yml file

--- fccs/settings.py
+++ fccs/settings.py
@@ -181,9 +181,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Custom User Model
 AUTH_USER_MODEL = 'users.User'
